46d331a3d9feaa70b04660b3effa2f72addd7096
ZeinabJahanbakhsh <jahanbakhsh.zeinab@gmail.com>
2023-11-21 17:50:51 +0330

    modified route

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(ListAdvertisementController::class)->group(function () {
     Route::get('/', 'index')->name('list-advertisements');
-    Route::get('show-job/{advertisement}', [ListAdvertisementController::class, 'show'])->name('show-job');
+    Route::get('show-job/{advertisement}', 'show')->name('show-job');
 });
 
 
